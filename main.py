9652bc14b5d6817f6d670ff08df63dcf25990a92
john <john@jpatmore.plus.com>
2021-01-02 15:15:33 +0000

    Quotes working

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
+    data = requests.get("https://api.kanye.rest", timeout=1)
+    quote = data.json()["quote"]
+    canvas.itemconfig(quote_text, text=quote)
 
 
 window = Tk()
@@ -21,6 +22,6 @@ kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
-
+get_quote()
 
 window.mainloop()
